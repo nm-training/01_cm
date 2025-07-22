7acea0bb83d72a0a0859fbbbfd1f6a953be9d121
bjungbogati <8600091+bjungbogati@users.noreply.github.com>
2025-07-23 03:14:04 +0545

    Added changes

---
+++ cm.R
@@ -0,0 +1,15 @@
+# *******************************************************************************************************
+# Program Name:                 cm.R
+# Project:                      TRA-025
+# Purpose:                      Create SDTM Dataset of CM
+# Original Author:              Binod Jung Bogati
+# Date Created:                 7/23/2025
+# Parameters:                   NA
+#
+# Input:                        raw_cm dm
+# Output:                       cm
+# Modifications:
+#   Date        By             Changes
+# **********  *************  *************************************
+#   
+# ********************************************************************************************************/
---
+++ exercises.R
@@ -0,0 +1,25 @@
+# load
+
+
+load(url("https://bit.ly/44UDUFG"))
+
+# Exercise 1: Write a program to map proper formats for CMSTDTC and CMENDTC from raw CM dataset. 
+
+
+
+# Exercise 2: Write a program to create Unique Subject Identifier variable and Sequence variable. 
+
+
+
+
+# Exercise 3: Write a program to calculate Study Day variable. (CMSTDTC, CMENDTC) 
+
+
+
+
+# Exercise 4: Take the concomitant medication data. 
+# The sample source data also has ATC coded variables for the medication names.
+# Plan the mapping of these into cm_mapping.xlsx excel file
+
+
+
