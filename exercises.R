eae460aec71c836d2d4852b92499d3eafe42fc73
bjungbogati <8600091+bjungbogati@users.noreply.github.com>
2025-07-23 07:06:33 +0545

    Added changes

--- exercises.R
+++ exercises.R
@@ -22,4 +22,9 @@ load(url("https://bit.ly/44UDUFG"))
 # Plan the mapping of these into cm_mapping.xlsx excel file
 
 
+# Exercise 5: Compare the mapping you proposed for exercise 4 with the mapping file given by Binod.
 
+
+# 
+# Exercise 6. After completing the comparison, create CM and SUPPCM datasets with a single cm.sas program. After that, validate the CM & SUPPCM datasets with provided dataset (will be available in SDTM folder upon request). Finally, save your program.  
+# 
